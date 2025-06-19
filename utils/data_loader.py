d41b2b44232623065cef686940529bf2effa8cf8
etfapp <smile871024@gmail.com>
2025-06-20 04:56:54 +0800

    Add files via upload

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -7,3 +7,14 @@ def load_etf_summary():
         return pd.read_csv(path)
     except Exception as e:
         return pd.DataFrame(columns=["代碼", "名稱", "殖利率", "價格", "技術燈號"])
+
+def load_watchlist():
+    path = os.path.join("data", "watchlist.csv")
+    try:
+        return pd.read_csv(path)
+    except:
+        return pd.DataFrame(columns=["代碼"])
+
+def save_watchlist(df):
+    path = os.path.join("data", "watchlist.csv")
+    df.to_csv(path, index=False)
